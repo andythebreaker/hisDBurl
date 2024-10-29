89c58bf382852b062a317666c4e8c744a18cf0ef
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2024-10-29 21:11:35 +0800

    加入專案檔案。

---
+++ hisDBurl/Ribbon1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace hisDBurl
+{
+    partial class hisDBurlGUI : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public hisDBurlGUI()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 元件設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.logi = this.Factory.CreateRibbonGroup();
+            this.setAllText = this.Factory.CreateRibbonButton();
+            this.ahcmsG = this.Factory.CreateRibbonGroup();
+            this.ahcmsGenUrl = this.Factory.CreateRibbonButton();
+            this.ndapG = this.Factory.CreateRibbonGroup();
+            this.ndapGenUrl = this.Factory.CreateRibbonButton();
+            this.ahtwhG = this.Factory.CreateRibbonGroup();
+            this.ahtwhGenUrl = this.Factory.CreateRibbonButton();
+            this.httpsGetPost = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.logi.SuspendLayout();
+            this.ahcmsG.SuspendLayout();
+            this.ndapG.SuspendLayout();
+            this.ahtwhG.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.logi);
+            this.tab1.Groups.Add(this.ahcmsG);
+            this.tab1.Groups.Add(this.ndapG);
+            this.tab1.Groups.Add(this.ahtwhG);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // logi
+            // 
+            this.logi.Items.Add(this.setAllText);
+            this.logi.Items.Add(this.httpsGetPost);
+            this.logi.Label = "庶務";
+            this.logi.Name = "logi";
+            // 
+            // setAllText
+            // 
+            this.setAllText.Label = "文字化";
+            this.setAllText.Name = "setAllText";
+            this.setAllText.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.setAllText_Click);
+            // 
+            // ahcmsG
+            // 
+            this.ahcmsG.Items.Add(this.ahcmsGenUrl);
+            this.ahcmsG.Label = "國史館檔案史料文物查詢系統";
+            this.ahcmsG.Name = "ahcmsG";
+            // 
+            // ahcmsGenUrl
+            // 
+            this.ahcmsGenUrl.Label = "加超連結";
+            this.ahcmsGenUrl.Name = "ahcmsGenUrl";
+            this.ahcmsGenUrl.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ahcmsGenUrl_Click);
+            // 
+            // ndapG
+            // 
+            this.ndapG.Items.Add(this.ndapGenUrl);
+            this.ndapG.Label = "臺灣省議會史料總庫";
+            this.ndapG.Name = "ndapG";
+            // 
+            // ndapGenUrl
+            // 
+            this.ndapGenUrl.Label = "加超連結";
+            this.ndapGenUrl.Name = "ndapGenUrl";
+            this.ndapGenUrl.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ndapGenUrl_Click);
+            // 
+            // ahtwhG
+            // 
+            this.ahtwhG.Items.Add(this.ahtwhGenUrl);
+            this.ahtwhG.Label = "國史館臺灣文獻館典藏管理系統";
+            this.ahtwhG.Name = "ahtwhG";
+            // 
+            // ahtwhGenUrl
+            // 
+            this.ahtwhGenUrl.Label = "加超連結";
+            this.ahtwhGenUrl.Name = "ahtwhGenUrl";
+            this.ahtwhGenUrl.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ahtwhGenUrl_Click);
+            // 
+            // httpsGetPost
+            // 
+            this.httpsGetPost.Label = "取得影像連結";
+            this.httpsGetPost.Name = "httpsGetPost";
+            this.httpsGetPost.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // hisDBurlGUI
+            // 
+            this.Name = "hisDBurlGUI";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.logi.ResumeLayout(false);
+            this.logi.PerformLayout();
+            this.ahcmsG.ResumeLayout(false);
+            this.ahcmsG.PerformLayout();
+            this.ndapG.ResumeLayout(false);
+            this.ndapG.PerformLayout();
+            this.ahtwhG.ResumeLayout(false);
+            this.ahtwhG.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup ahcmsG;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ahcmsGenUrl;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup logi;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton setAllText;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup ndapG;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup ahtwhG;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ndapGenUrl;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ahtwhGenUrl;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton httpsGetPost;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal hisDBurlGUI Ribbon1
+        {
+            get { return this.GetRibbon<hisDBurlGUI>(); }
+        }
+    }
+}
---
+++ hisDBurl/Ribbon1.cs
@@ -0,0 +1,222 @@
+﻿using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace hisDBurl
+{
+    public partial class hisDBurlGUI
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        }
+
+        private void setAllText_Click(object sender, RibbonControlEventArgs e)
+        {
+            var excelApp = Globals.ThisAddIn.Application;
+            var worksheet = excelApp.ActiveSheet as Microsoft.Office.Interop.Excel.Worksheet;
+
+            if (worksheet != null)
+            {
+                // Set the format of all cells in the worksheet to Text
+                worksheet.Cells.NumberFormat = "@";
+            }
+        }
+
+        private void addUrl(Int16 colNum, string qry1, string qry2, string url1, string url2) {
+            // Get the active worksheet
+            Worksheet worksheet = Globals.ThisAddIn.Application.ActiveSheet as Worksheet;
+
+            if (worksheet == null) return;
+
+            // Loop through each row until the last used row
+            int lastRow = worksheet.Cells[worksheet.Rows.Count, 1].End(XlDirection.xlUp).Row;
+
+            for (int row = 1; row <= lastRow; row++)
+            {
+                Range firstCell = worksheet.Cells[row, 1];
+                Range secondCell = worksheet.Cells[row, colNum];//2];
+
+                // Check if the first cell can be converted to a number
+                if (double.TryParse(firstCell.Text.ToString(), out double numberValue))
+                {
+                    // Get the text of the second column in the same row
+                    string secondColText = secondCell.Text.ToString();
+
+                    // Encode the query in Base64 format
+                    string encodedQuery = Convert.ToBase64String(
+                        System.Text.Encoding.UTF8.GetBytes(
+                            //$"{{\"query\":[{{\"field\":\"in_store_no\",\"value\":\"{secondColText}\"}}]}}"
+                            qry1 + secondColText+ qry2
+                        )
+                    );
+
+                    // Create the URL with the encoded query
+                    string url = url1+ encodedQuery+ url2;//$"https://ahonline.drnh.gov.tw/index.php?act=Archive/search/{encodedQuery}%3D%3D";
+
+                    // Find the last used column in this row and set the URL in the next column
+                    //int lastColumn = worksheet.Cells[row, worksheet.Columns.Count].End(XlDirection.xlToLeft).Column;
+                    //worksheet.Cells[row, lastColumn + 1].Value = url;
+
+                    // Find the last used column in this row and set the hyperlink in the next column
+                    int lastColumn = worksheet.Cells[row, worksheet.Columns.Count].End(XlDirection.xlToLeft).Column;
+                    Range targetCell = worksheet.Cells[row, lastColumn + 1];
+
+                    // Add the hyperlink to the target cell
+                    worksheet.Hyperlinks.Add(targetCell, url, Type.Missing, "Click to view archive", url);
+                }
+            }
+        }
+
+        private void ahcmsGenUrl_Click(object sender, RibbonControlEventArgs e)
+        {
+            addUrl(2, "{\"query\":[{\"field\":\"in_store_no\",\"value\":\"", "\"}]}", "https://ahonline.drnh.gov.tw/index.php?act=Archive/search/", "%3D%3D");
+        }
+
+        private void ndapGenUrl_Click(object sender, RibbonControlEventArgs e)
+        {
+            addUrl(3, "{\"query\":[{\"field\":\"identifier\",\"value\":\"", "\"}]}", "https://drtpa.th.gov.tw/index.php?act=Archive/search/", "%3D%3D");
+        }
+
+        private void ahtwhGenUrl_Click(object sender, RibbonControlEventArgs e)
+        {
+            addUrl(3, "{\"search\":[{\"field\":\"list_dataid\",\"value\":\"", "\"}]}", "https://onlinearchives.th.gov.tw/index.php?act=Archive/search/undefined/", "%3D");
+        }
+
+        public static string GeneratePhpSessionId(int length = 32)
+        {
+            // Create a byte array to hold the random bytes
+            byte[] randomBytes = new byte[length];
+
+            // Fill the array with cryptographically secure random bytes
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(randomBytes);
+            }
+
+            // Convert the byte array to a hexadecimal string
+            StringBuilder sessionId = new StringBuilder(length * 2);
+            foreach (byte b in randomBytes)
+            {
+                sessionId.Append(b.ToString("x2")); // Convert to hex
+            }
+
+            return sessionId.ToString();
+        }
+
+        private async Task<JObject> FetchAccKeyAndPost(string url, string phpSessionId)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Set the PHPSESSID cookie in the header
+                    client.DefaultRequestHeaders.Add("Cookie", $"PHPSESSID={phpSessionId}");
+                    // Make the HTTP GET request
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    // Read the response body as a string
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    // Use regex to find the acckey
+                    string pattern = @"acckey='(?<key>[^']+)'";
+                    Match match = Regex.Match(responseBody, pattern);
+                    if (match.Success)
+                    {
+                        string acckey = match.Groups["key"].Value;
+                        // Prepare the POST request
+                        var postData = new System.Collections.Generic.Dictionary<string, string>
+                {
+                    { "act", $"Display/initial/{acckey}" }
+                };
+                        var content = new FormUrlEncodedContent(postData);
+                        client.DefaultRequestHeaders.Referrer = new Uri(url);
+                        // Send the POST request
+                        HttpResponseMessage postResponse = await client.PostAsync(url, content);
+                        postResponse.EnsureSuccessStatusCode();
+                        // Get the response body as JSON
+                        string jsonResponse = await postResponse.Content.ReadAsStringAsync();
+                        // Optionally parse the JSON
+                        JObject json = JObject.Parse(jsonResponse);
+                        return json;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred: {ex.Message}", "Error");
+                    return null;
+                }
+            }
+        }
+
+        private async Task ProcessSheet(Microsoft.Office.Interop.Excel.Worksheet worksheet)
+        {
+            Microsoft.Office.Interop.Excel.Range usedRange = worksheet.UsedRange;
+
+            for (int row = 1; row <= usedRange.Rows.Count; row++)
+            {
+                for (int col = 1; col <= usedRange.Columns.Count; col++)
+                {
+                    Microsoft.Office.Interop.Excel.Range cell = worksheet.Cells[row, col] as Microsoft.Office.Interop.Excel.Range;
+                    string cellText = cell.Text.ToString();
+
+                    if (Uri.IsWellFormedUriString(cellText, UriKind.Absolute))
+                    {
+                        JObject resJson = await FetchAccKeyAndPost(cellText, GeneratePhpSessionId());
+
+                        if (resJson != null)
+                        {
+                            Microsoft.Office.Interop.Excel.Range rightCell = worksheet.Cells[row, col + 1] as Microsoft.Office.Interop.Excel.Range;
+                            rightCell.Value2 = ProcessJsonObject(resJson, cellText);//resJson.ToString();
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+private string ProcessJsonObject(JObject obj,string url)
+    {
+        if ((int)obj["action"] == 0)
+        {
+            return obj["info"].ToString();
+        }
+        else if ((int)obj["action"] == 1)
+        {
+            var uri = new Uri(url);
+            string domain = uri.Host;
+            string display = obj["data"]["display"].ToString();
+            string resource = obj["data"]["resouse"].ToString();
+            return $"https://{domain}/index.php?act=Display/{display}/{resource}";
+        }
+        else
+        {
+            throw new ArgumentException("Invalid action type");
+        }
+    }
+
+
+    private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            var excelApp = Globals.ThisAddIn.Application;
+            var worksheet = excelApp.ActiveSheet as Microsoft.Office.Interop.Excel.Worksheet;
+            _ = ProcessSheet(worksheet);
+        }
+    }
+}
